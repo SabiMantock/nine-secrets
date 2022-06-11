959a9a43bb7e31ee0f7803ca93c4a719a1a037c7
Sabi Mantock <sabimantock@gmail.com>
2022-06-11 14:51:36 +0000

    created sign up component, form input component, button component

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -3,7 +3,8 @@ import {
   getAuth,
   signInWithRedirect,
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from 'firebase/auth'
 import { getFirestore, getDoc, setDoc, doc } from 'firebase/firestore'
 
@@ -28,9 +29,15 @@ provider.setCustomParameters({
 export const auth = getAuth()
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, provider)
+
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async userAuth => {
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return
   const userDocRef = doc(db, 'users', userAuth.uid)
 
   const userSnapshot = await getDoc(userDocRef)
@@ -43,12 +50,19 @@ export const createUserDocumentFromAuth = async userAuth => {
       await setDoc(userDocRef, {
         displayName,
         email,
-        createdAt
+        createdAt,
+        ...additionalInformation
       })
     } catch (error) {
-      console.log('error creating user', error)
+      console.log('error creating user', error.message)
     }
   }
 
   return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return
+
+  return await createUserWithEmailAndPassword(auth, email, password)
 }
