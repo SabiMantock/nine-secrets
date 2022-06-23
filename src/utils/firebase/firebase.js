4593d10fef708dc16a31126b4e63ad806de82234
Sabi Mantock <sabimantock@gmail.com>
2022-06-23 18:03:15 +0000

    fixed sign-in issue

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -93,7 +93,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef
+  return userSnapshot
 }
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
