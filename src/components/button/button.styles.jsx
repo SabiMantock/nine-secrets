b317033899c0a57387660e28a6620a2d39973e89
Sabi Mantock <sabimantock@gmail.com>
2022-06-26 23:24:34 +0000

    integrated payment

--- src/components/button/button.styles.jsx
+++ src/components/button/button.styles.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const BaseButton = styled.button`
   min-width: 165px;
@@ -11,19 +11,20 @@ export const BaseButton = styled.button`
   background-color: black;
   color: white;
   text-transform: uppercase;
-  font-family: "Open Sans Condensed";
+  font-family: 'Open Sans Condensed';
   font-weight: bolder;
   border: none;
   cursor: pointer;
   display: flex;
   justify-content: center;
+  align-items: center;
 
   &:hover {
     background-color: white;
     color: black;
     border: 1px solid black;
   }
-`;
+`
 
 export const GoogleSignInButton = styled(BaseButton)`
   background-color: #4285f4;
@@ -33,7 +34,7 @@ export const GoogleSignInButton = styled(BaseButton)`
     background-color: #357ae8;
     border: none;
   }
-`;
+`
 
 export const InvertedButton = styled(BaseButton)`
   background-color: white;
@@ -45,4 +46,25 @@ export const InvertedButton = styled(BaseButton)`
     color: white;
     border: none;
   }
-`;
+`
+
+export const ButtonSpinner = styled.div`
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  border: 3px solid rgba(195, 195, 195, 0.6);
+  border-radius: 50%;
+  border-top-color: #636767;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-animation: spin 1s ease-in-out infinite;
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+`
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
+import { Elements } from '@stripe/react-stripe-js'
 
 import App from './App'
 
 import { persistor, store } from './store/store'
 
 import './index.scss'
+import { stripePromise } from './utils/stripe/stripe.utils'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
@@ -16,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
